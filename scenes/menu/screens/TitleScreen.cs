e8262e5d0630fb33914b62084546fc07ea865a53
Goniix <dragonixgold@gmail.com>
2025-08-30 11:52:06 +0200

    Tree refactor

--- scenes/menu/screens/TitleScreen.cs
+++ scenes/menu/screens/TitleScreen.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 using Quad.scripts;
 
@@ -9,11 +10,54 @@ public partial class TitleScreen : Control
 {
     [Export] private Button _hostButton;
     [Export] private Button _joinButton;
+    [Export] private LineEdit _nameEdit;
 
+    private string[] _defaultNames =
+    [
+        "WoodRaptor",
+        "EpicSteel",
+        "ViperPandora",
+        "CookieSniper",
+        "SwordCraby",
+        "MoonWolf",
+        "CrabyDoor",
+        "DeathShield",
+        "DoctorAim",
+        "DeathLotus",
+        "BadSword",
+        "SmallMorphine",
+        "EpicDragon",
+        "NightMilo",
+        "FreezShadow",
+        "FlyingBronze",
+        "WarDream",
+        "EpicCheat",
+        "BetaIron",
+        "WolfGt",
+        "BreadCar",
+        "LuckOmega",
+        "NeverRiku",
+        "RexEagle"
+    ];
+    
     public override void _Ready()
     {
         _hostButton!.Pressed += OnCreateLobbyButtonPressed;
         _joinButton!.Pressed += OnJoinLobbyButtonPressed;
+        _nameEdit.TextChanged += SaveName;
+
+        SetRandomDefaultName();
+    }
+
+    private void SetRandomDefaultName()
+    {
+        _nameEdit.Text = Random.Shared.GetItems(_defaultNames, 1)[0];
+        SaveName(_nameEdit.Text);
+    }
+    
+    private void SaveName(string text)
+    {
+        OnlineLobby.Instance.LocalPlayerInfo.Name = text;
     }
     
     private void OnCreateLobbyButtonPressed()
